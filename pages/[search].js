beb4223ee23996d2c157f0e739e2097d2c5b4b63
Akanni David Seun <akannidavidseun@gmail.com>
2020-09-01 16:40:22 +0100

    fixed search issue

--- pages/[search].js
+++ pages/[search].js
@@ -3,26 +3,27 @@ import { Card, MainReviews, ReviewButton, Progress } from 'components/blocks'
 import DefaultLayout from 'components/layout/DefaultLayout'
 import { useRouter } from 'next/router'
 import APIClient from 'utils/APIClient'
+import { IDLE, PENDING, RESOLVED, REJECTED } from '@constants'
 
 const Search = () => {
   const { query } = useRouter()
-  const [loading, loadingSet] = useState(true)
+  const [fetchState, setFetchState] = useState(IDLE)
   const [reviews, reviewsSet] = useState([])
 
   useEffect(() => {
+    setFetchState(PENDING)
     if (query.company) {
       APIClient.post('reviews/search', { search: query.company })
         .then(response => {
-          loadingSet(false)
+          setFetchState(RESOLVED)
           if (response.data.statusCode === 200) {
             reviewsSet(response.data.data)
           }
         })
         .catch(_error => {
+          setFetchState(REJECTED)
           loadingSet(false)
         })
-    } else {
-      loadingSet(false)
     }
 
     return () => {
@@ -33,7 +34,7 @@ const Search = () => {
     <DefaultLayout>
       <main className="w-screen pt-5 pb-16 px-4 xl:px-0">
         <div className="sm:max-w-full mx-auto md:max-w-screen-lg">
-          { !loading && reviews && reviews.length > 0 &&
+          { fetchState === RESOLVED && reviews && reviews.length > 0 &&
           (
             <div className="grid grid-cols-1 md:grid-cols-3 md:gap-4">
               <div className="col-span-2">
@@ -52,11 +53,14 @@ const Search = () => {
               </div>
             </div>
           )}
-          { !loading && (!reviews || reviews.length < 1) &&
+          { (fetchState === RESOLVED || fetchState === REJECTED) && (!reviews || reviews.length < 1) &&
             <p className="text-3xl text-white text-center mt-32">
               <span>😬😬</span>
-              <br/> No review found {query.company && 'for'} {query.company}</p>}
-          { loading && <Progress />}
+              <br/> 
+              No review found {query.company && 'for'} {query.company}
+            </p>
+          }
+          { (fetchState === IDLE || fetchState === PENDING )&& <Progress />}
         </div>
       </main>
     </DefaultLayout>
